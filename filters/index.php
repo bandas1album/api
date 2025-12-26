8e7e991399f0585f5801f205e075b046f2153957
Kevin Oliveira <me@kevinoliveira.com.br>
2025-12-25 21:40:15 -0300

    Refactor: add artist on search

--- filters/index.php
+++ filters/index.php
@@ -3,3 +3,24 @@
 add_filter('rest_url_prefix', function() {
   return 'json';
 });
+
+add_filter('posts_search', function ($search, $query) {
+  global $wpdb;
+
+  if (!$query->get('s')) return $search;
+
+  $q = esc_sql($query->get('s'));
+
+  return "
+    AND (
+      {$wpdb->posts}.post_title LIKE '%{$q}%'
+      OR {$wpdb->posts}.post_content LIKE '%{$q}%'
+      OR EXISTS (
+        SELECT 1 FROM {$wpdb->postmeta}
+        WHERE post_id = {$wpdb->posts}.ID
+        AND meta_key = 'artist'
+        AND meta_value LIKE '%{$q}%'
+      )
+    )
+  ";
+}, 10, 2);
